ec2ea7432cbb07c40ad7ffab43456bdba182f2f8
erytan <ntan1695123@gmail.com>
2024-07-04 12:02:39 +0700

    done router user

---
+++ server/routes/user.js
@@ -0,0 +1,24 @@
+const router = require('express').Router()
+const ctrls = require('../controllers/user')
+const { verifyAccessToken, isAdmin } = require('../middlewares/verifyToken')
+
+router.post('/import',ctrls.importExcel)
+router.post('/login', ctrls.login)
+router.get('/current', verifyAccessToken, ctrls.getCurrent)
+router.post('/refreshtoken', ctrls.refreshAccessToken)
+router.get('/logout', ctrls.logout)
+router.get('/forgetpassword', ctrls.forgetPassword)
+router.put('/resetpassword', ctrls.resetPassword)
+
+
+
+router.put('/current', [verifyAccessToken], ctrls.updateUser)
+router.put('/address', [verifyAccessToken], ctrls.updateUserAddress)
+
+router.get('/', [verifyAccessToken], ctrls.getUser)
+router.delete('/', [verifyAccessToken, isAdmin], ctrls.deleteUser)
+router.put('/:uid', [verifyAccessToken, isAdmin], ctrls.updateUserByAdmin)
+
+module.exports = router
+
+// CRUD | Create - Read - Update - Delete | POST - GET -PUT - DELETE
