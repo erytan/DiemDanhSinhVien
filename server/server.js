30d9295c7cfeb3a2851fd2ca77564e41abb1bc51
erytan <ntan1695123@gmail.com>
2024-07-04 12:00:52 +0700

    update server

--- server/server.js
+++ server/server.js
@@ -1,16 +1,22 @@
 const express = require('express')
 require('dotenv').config()
-
-
+const dbconnect = require('./config/dbconnect')
+const initRoutes = require('./routes')
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
 
 const app = express()
 const port = process.env.PORT || 8000
-app.use(express.json())
+app.use(cookieParser())
+app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: '*'
+}));
 
-app.use('/', (req, res) => {
-    res.send("test")
-})
+dbconnect()
+initRoutes(app)
+
 app.listen(port, () => {
-    console.log('tesstt : ' + port);
+    console.log('Hehehe cố gắng lên ');
 })
