0848e3a12cb578c7965a93251332d646ee712c08
erytan <ntan1695123@gmail.com>
2024-07-05 18:10:36 +0700

    router subject

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,9 @@
 const { notFound, errHandler } = require('../middlewares/errHandler')
 const userRouter = require('./user')
+const subjectRouter = require('./subject')
 const initRoutes = (app) => {
     app.use('/api/user', userRouter)
-
+    app.use('/api/subject',subjectRouter)
 
     app.use(notFound)
     app.use(errHandler)
