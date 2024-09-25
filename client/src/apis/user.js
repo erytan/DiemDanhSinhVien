2022b828a5f9b2cb10ec08542daadd3214c3b03b
erytan <ntan1695123@gmail.com>
2024-09-25 19:56:54 +0700

    api user

--- client/src/apis/user.js
+++ client/src/apis/user.js
@@ -1,12 +1,26 @@
 import axios from "../axios";
-export const apiLogin = (data) => axios.post('user/login', data);
-export const apiGetUSER = (data) => axios({
-    url: 'user/current',
-    method: 'get',
-    data
-})
-export const apiGetAllUSER = (data) => axios({
-    url: 'user/',
-    method: 'get',
-    data
-})
+export const apiLogin = (data) => axios.post("user/login", data);
+export const apiGetUSER = (data) =>
+  axios({
+    url: "user/current",
+    method: "get",
+    data,
+  });
+export const apiGetAllUSER = (data) =>
+  axios({
+    url: "user/",
+    method: "get",
+    data,
+  });
+  export const apiForgotPassword = (data) =>
+    axios({
+      url: "user/forgetpassword",
+      method: "post",
+      data,
+    });
+  export const apiResetPassword  = (data) =>
+    axios({
+      url: "user/resetpassword",
+      method: "put",
+      data,
+    });
