a9ccfff1852dbfe213308465886b80a1ffcb3eb4
erytan <ntan1695123@gmail.com>
2024-09-25 20:10:35 +0700

    Update model session

--- server/models/session.js
+++ server/models/session.js
@@ -1,41 +1,45 @@
-const mongoose = require("mongoose");
+  const mongoose = require("mongoose");
 
-const sessionSchema = new mongoose.Schema(
-  {
-    id_session: {
-      type: String,
-      required: true,
-    },
-    subject: {
-      type: String,
-      ref: "Subject",
-    },
-    date: {
-      type: Date,
-      required: true,
-    },
-    startTime: {
-      type: String,
-      required: true,
-    },
-    endTime: {
-      type: String,
-      required: true,
-    },
-    location: {
-      type: String,
-      required: true,
-    },
-    user: [
-      {
+  const sessionSchema = new mongoose.Schema(
+    {
+      id_session: {
         type: String,
-        ref: "User",
+        required: true,
       },
-    ],
-  },
-  {
-    timestamps: true,
-  }
-);
+      subject: {
+        type: String,
+        ref: "Subject",
+      },
+      date: {
+        type: Date,
+        required: true,
+      },
+      startTime: {
+        type: String,
+        required: true,
+      },
+      endTime: {
+        type: String,
+        required: true,
+      },
+      location: {
+        type: String,
+        required: true,
+      },
+      user: [
+        {
+          firstName: String,
+          lastName: String,
+          className: String,
+          MSSV: String,
+          attendanceCount: { type: Number, default: 0 },
+          checkScanQR: { type: Boolean, default: false },
+        },
+      ],
+    },
+    {
+      timestamps: true,
+    }
+  );
 
-module.exports = mongoose.model("Session", sessionSchema);
+  module.exports = mongoose.model("Session", sessionSchema);
